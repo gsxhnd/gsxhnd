79dc815553d7ac1505f1946ce19e5b7febd077f8
gsxhnd <gsxhnd@gmail.com>
2026-03-06 14:30:40 +0800

    updaye

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,10 +3,9 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://stargazers.club",
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap()],
 });
